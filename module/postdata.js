523a0710b44bc671ad3eb0194c630afc0e837695
NORTH <north@localhost.localdomain>
2015-12-01 23:31:23 +0800

    wechat

--- module/postdata.js
+++ module/postdata.js
@@ -7,6 +7,26 @@ var xml_bodyparser = require("express-xml-bodyparser");
 
 config.app.use(xml_bodyparser());
 
+
+
+
+
+var text = function(ok, req, res, result) {
+	console.log("text");
+	res.send("");
+}
+
+var defaultMsg = function(ok, req, res, result) {
+	console.log(result);
+	res.send("");
+}
+
+
+
+exports.text = text;
+exports.defaultMsg = defaultMsg;
+
+
 function getPost(req, res, next) {
 	if (!req.body) {
 		//e
@@ -26,10 +46,10 @@ function getPost(req, res, next) {
 
 	switch (data.msgtype[0]) {
 		case 'text':
-			text(true, req, res, data);
+			exports.text(true, req, res, data);
 			break;
 		default:
-			defaultMsg(true, req, res, data);
+			exports.defaultMsg(true, req, res, data);
 
 	}
 
@@ -38,19 +58,4 @@ function getPost(req, res, next) {
 }
 
 
-
-var text = function(ok, req, res, result) {
-	console.log("text");
-	res.send("");
-}
-
-var defaultMsg = function(ok, req, res, result) {
-	console.log(result);
-	res.send("");
-}
-
-
 config.app.post('/', getPost);
-
-exports.text = text;
-exports.defaultMsg = defaultMsg;
